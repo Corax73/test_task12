a5b2adbb432dadc08b89efe8a51bbb358bc98f5b
Corax73 <ramaz999@inbox.ru>
2024-04-14 22:52:14 +0400

    added tests

--- tests/ModelsTests/GroupModelTest.php
+++ tests/ModelsTests/GroupModelTest.php
@@ -5,6 +5,7 @@ namespace Tests\ModelsTests;
 require_once 'config/const.php';
 
 use Models\Group;
+use Models\GroupRights;
 use PHPUnit\Framework\TestCase;
 
 class GroupModelTest extends TestCase
@@ -46,5 +47,32 @@ class GroupModelTest extends TestCase
     public function testFindWithValidGroupId(): void
     {
         $this->assertIsArray($this->group->find($this->validGroupId));
+    }
+
+    public function testAllByContainedKeys(): void
+    {
+        $data = $this->group->all();
+        $groupKeys = ['id' => 'test', 'title' => 'test', 'created_at' => 'test', 'updated_at' => 'test'];
+        $this->assertTrue(count(array_diff_key($groupKeys, $data[0])) === 0);
+    }
+
+    public function testSave(): void
+    {
+        $this->group->save('php-unit');
+        $data = $this->group->getByTitle('php-unit');
+        $this->group->delete($data[0]);
+        $this->assertTrue(count($data) > 0 && in_array('php-unit', $data));
+    }
+
+    public function testRightsWithInvalidGroupId(): void
+    {
+        $this->assertTrue(count($this->group->rights($this->invalidGroupId)) == 0);
+    }
+
+    public function testRightsWithValidGroupId(): void
+    {
+        $groupRights = new GroupRights();
+        $idGroupWithRights = $groupRights->all(1)[0]['group_id'];
+        $this->assertTrue(count($this->group->rights($idGroupWithRights)) > 0);
     }
 }
